f22bd8e8f9cc449730089652ca212d76bb9dea8f
Anastasiia Moroziuk <lucius2305@gmail.com>
2021-12-12 20:39:00 +0200

    added project files

---
+++ Files.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Files
+    {
+        public void DisplayTable<T>(T[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void DisplayArr<T>(T[] arr)
+
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("");
+        }
+
+        public double[,] ReadTable(string filepath, int arrLength1, int arrLength2)
+        {
+            var table = new double[arrLength1, arrLength2];
+            var contents = File.ReadAllText(filepath).Split('\n');
+            var csv = from line in contents
+                      select line.Split(',').ToArray();
+
+            for (int i = 0; i < arrLength1; i++)
+            {
+                var csvList = csv.ToList();
+                for (int j = 0; j < arrLength2; j++)
+                {
+                    table[i, j] = Convert.ToDouble((csvList[i][j]).Replace(".", ","));
+                }
+            }
+
+            return table;
+        }
+
+        public int[,] Intify(double[,] arr)
+        {
+            var table = new int[arr.GetLength(0), arr.GetLength(1)];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    table[i, j] = (int)arr[i, j];
+                }
+            }
+
+            return table;
+        }
+
+        public double[] ToLine(double[,] arr, int line)
+        {
+            var list = new double[arr.GetLongLength(1)];
+            for (int i = 0; i < arr.GetLongLength(1); i++)
+            {
+                list[i] = arr[line, i];
+            }
+
+            return list;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static double[] K_array = new double[20];
+        static double[] M_array = new double[20];
+        static int[,] C = new int[20, 20];
+
+        static string fileTable = "C:/table_06.csv";
+        static string fileProb = "C:/prob_06.csv";
+
+        static double[] prob_C = new double[20];
+        static double[,] prob_MC = new double[20, 20];
+        
+        static void Main(string[] args)
+        {
+            var fileHelper = new Files();
+
+            var C_double = fileHelper.ReadTable(fileTable, 20, 20);
+            C = fileHelper.Intify(C_double);
+
+            var M_K = fileHelper.ReadTable(fileProb, 2, 20);
+            M_array = fileHelper.ToLine(M_K, 0);
+            K_array = fileHelper.ToLine(M_K, 1);
+
+
+            Console.ReadKey();
+        }
+    }
+}
